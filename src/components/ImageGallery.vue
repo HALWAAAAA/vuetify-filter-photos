b4c15218d553337112d3d463f621762aef08e785
HALWAAAAA <taras.kotsyuba2003@gmail.com>
2024-11-20 16:00:43 +0100

    Initial commit

---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card class="ma-5 my-2 pa-3">
+    <v-row>
+      <v-col cols="4" v-for="n in 200" sm="3" md="2" lg="2">
+        <v-card @click="copyUrl(`https://picsum.photos/500/300?image=${n * 5 + 10}${pr.isWithColor ? '' : '&grayscale'}`)">
+          <v-img
+            :src="`https://picsum.photos/500/300?image=${n * 5 + 10}${pr.isWithColor ? '' : '&grayscale'}`"
+            aspect-ratio="1"
+            cover
+            :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}${pr.isWithColor ? '' : '&grayscale'}`"
+          >
+            <template v-slot:placeholder>
+              <v-row align="center" class="fill-height ma-0" justify="center">
+                <v-progress-circular
+                  color="grey-lighten-5"
+                  indeterminate
+                ></v-progress-circular>
+              </v-row>
+            </template>
+          </v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+<script setup>
+const pr = defineProps(["isWithColor"]);
+const copyUrl =async (url)=>{
+    await navigator.clipboard.writeText(url)
+}
+</script>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-navigation-drawer expand-on-hover rail>
+    <v-list>
+      <v-list-item
+        prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+        subtitle="taras.kotsyuba2003@gmailcom"
+        title="Taras K"
+      ></v-list-item>
+    </v-list>
+    <v-divider></v-divider>
+<v-list density="compact" nav>
+    <v-list-item prepend-icon="mdi-image" title="Color Gallery" value="color" @click="selectOption(true)"></v-list-item>
+    <v-list-item prepend-icon="mdi-dialpad" title="Grayspace Gallery" value="color" @click="selectOption(false)"></v-list-item>
+</v-list>
+  </v-navigation-drawer>
+</template>
+
+<script setup>
+const emits = defineEmits(['selectOption']);
+
+const selectOption = (isColor) =>{
+    emits("selectOption", isColor)
+}
+</script>
